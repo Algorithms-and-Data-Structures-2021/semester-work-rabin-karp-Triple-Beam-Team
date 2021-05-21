237bede19bb7e393f16006584645183f458f5071
Bogdan Abarikhin <bogdan_abarihin@mail.ru>
2021-05-21 18:55:53 +0400

    refactored bench

--- benchmark/search_for_substring.cpp
+++ benchmark/search_for_substring.cpp
@@ -21,8 +21,8 @@ string folders[10] = {"/data_1/",
                       "/data_4/", "/data_5/", "/data_6/",
                       "/data_7/", "/data_8/", "/data_9/",
                       "/data_10/"};
-string files[12] = {"100.csv", "500.csv", "1000.csv", "5000.csv", "10000.csv", "25000.csv", "50000.csv", "100000csv",
-                    "250000.csv", "500000.csv", "750000.csv", "1000000.csv"};
+string files[14] = {"100.csv", "500.csv", "1000.csv", "5000.csv", "10000.csv", "25000.csv", "50000.csv", "100000csv",
+                    "250000.csv", "500000.csv", "750000.csv", "1000000.csv", "2500000.csv", "5000000.csv"};
 
 int main() {
   // работа с набором данных
@@ -51,9 +51,10 @@ int main() {
         auto time_diff = time_point_after - time_point_before;
         long time_elapsed_ns_search = chrono::duration_cast<chrono::nanoseconds>(time_diff).count();
 
-        cout << time_elapsed_ns_search;
+        cout << time_elapsed_ns_search << "\n";
         string_file = "";
       }
     }
+   cout << "\n";
   }
 }
--- main.cpp
+++ main.cpp
@@ -16,9 +16,20 @@ int main(int argc, char *argv[]) {
                      "Because my love is here\n"
                      "\n"
                      "Performed by maestro E.Ponasenkov";
-  std::string pattern = "Every moment of the year\n"
+  std::string pattern = "Every time i look down on this timeless town,\n"
+                        "Whether blue or gray be her skies,\n"
+                        "Whether loud be her cheers, or whether soft be her tears,\n"
+                        "More and more do I realize that...\n"
+                        "I love Paris in the spring time\n"
+                        "I love Paris in the fall\n"
+                        "I love Paris in the summer when it sizzles\n"
+                        "I love Paris in the winter when it drizzles\n"
+                        "I love Paris every moment\n"
+                        "Every moment of the year\n"
                         "I love Paris, why oh why do I love Paris\n"
-                        "Because my love is here\n";
+                        "Because my love is here\n"
+                        "\n"
+                        "Performed by maestro E.Ponasenkov";
   std::vector<int> insertions = itis::search(pattern, text);
   for (int elem: insertions) {
     std::cout << elem << std::endl;
